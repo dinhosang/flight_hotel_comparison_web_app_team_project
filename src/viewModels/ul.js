2dbb2cb67430ed44b75dbaf185648411f3d4b0ed
Richard Allison <richard.allison.18@outlook.com>
2018-03-02 16:47:12 +0000

    Ul viewModel created, but not yet tested

---
+++ src/viewModels/ul.js
@@ -0,0 +1,33 @@
+const Ul = function(details) {
+  this.depart   = details.depart;
+  this.return   = details.return;
+  this.flights  = details.flights;
+  this.searchResultView = document.getElementsById('destination-list');
+
+  this.addTitle();
+  this.populateView();
+}
+
+Ul.prototype.addTitle = function() {
+  const title = document.createElement('h2');
+  title.id    = 'destination-list-title';
+  title.innerText = `Destinations available for depart date:
+                    ${this.depart}, returning: ${this.return}`;
+  this.searchResultView.appendChild(title);
+};
+
+Ul.prototype.populateView = function() {
+  this.flights.forEach(destination => this.addDestination(destination));
+};
+
+Ul.prototype.addDestination = function(destination) {
+  const destinationUl = document.createElement('ul');
+  destinationUl.classList.add('random-destination-item');
+  const destinationLi = document.createElement('li');
+  const priceLi = document.createElement('li');
+  destinationLi.innerText = destination.destination;
+  priceLi.innerText = destination.price;
+  destinationUl.appendChild(destinationLi);
+  destinationUl.appendChild(priceLi);
+  this.searchResultView.appendChild(destinationUl);
+}
