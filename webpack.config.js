3c991efdbcbc5c1cbe4b09bccc9026abb5d253b5
Richard Allison <richard.allison.18@outlook.com>
2018-03-02 14:49:45 +0000

    installed modules, written .gitignore, and scripted bundle.js into index.html

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+config = {
+  entry: __dirname + "/src/app.js",
+  output: {
+    filename: "bundle.js",
+    path: __dirname + "/build"
+  },
+  devtool: "source-map",
+  mode: "development"
+}
+
+module.exports = config;
