3da2155503f6abe42475d8c9a135a7b8077cfb66
Richard Allison <richard.allison.18@outlook.com>
2018-03-02 16:23:31 +0000

    built functionality for a formView Model: listening to button click and posting information to a Ul view

--- src/app.js
+++ src/app.js
@@ -1,6 +1,30 @@
 const main = function() {
-  console.log('Page loaded');
+
+  console.log('page loaded');
+
+  const submitButton = document.getElementById('submit-random-search');
+  submitButton.addEventListener('click', listFlights);
+}
+
+const listFlights = function() {
+  const Ul = require('./viewModels/Ul');
+
+  console.log('button clicked');
+
+  const departDateInput = document.getElementById('depart-date-input-for-random-search');
+  const returnDateInput = document.getElementById('return-date-input-for-random-search');
+
+  const departDate = departDateInput.value;
+  const returnDate = returnDateInput.value;
+
+  const fakeFlightData = [{destination: 'EDI', price: 200},
+                          {destination: 'PAR', price: 100}];
+
+  const dataToPassToUL = {depart: departDate, return: returnDate,
+                          flights: fakeFlightData};
+
+  new Ul(dataToPassToUL);
 }
 
 
-document.addEventListener('DOMContentLoaded', main)
+document.addEventListener('DOMContentLoaded', main);
