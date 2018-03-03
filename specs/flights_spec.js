3147d7c8573cc4a22785acaf030bfc6a956b5c3e
Richard Allison <richard.allison.18@outlook.com>
2018-03-03 15:15:17 +0000

    fixed flights model so that I could continue to pass tests. Performed in this branch in error. Added a hotels class into dataModels by performing TDD

--- specs/flights_spec.js
+++ specs/flights_spec.js
@@ -13,4 +13,8 @@ describe('Flights', function() {
   it ('should return an array of flight details', function() {
     assert.deepStrictEqual(flights.allFlights instanceof Array, true);
   })
+
+  it('should return an array of flights details for a destination', function() {
+    assert.deepStrictEqual(flights.allFlightsToDestination instanceof Array, true);
+  })
 })
