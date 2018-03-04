44e7583b2ce1f8f8fff7c66b0740e51698f6a351
Richard Allison <richard.allison.18@outlook.com>
2018-03-04 12:51:02 +0000

    renamed ulViewModel to resultsView; made resultsView more eventdriven by removing information passed when view is invoked

---
+++ src/viewModels/resultsView.js
@@ -0,0 +1,29 @@
+const ResultsView = function() {
+  this.searchResultView = document.getElementById('results-view-section');
+
+  this.clearSearchResultView();
+}
+
+ResultsView.prototype.clearSearchResultView = function(){
+  this.searchResultView.innerHTML = '';
+}
+
+ResultsView.prototype.createDestinationsListView = function (details, callbackFunction) {
+  const options = {
+    "flights": details,
+    callback: function(){
+      callbackFunction(this)
+    }.bind(this)
+  }
+
+  const RandomDestinationsList = require('./randomDestinationsList.js');
+  new RandomDestinationsList(options);
+};
+
+ResultsView.prototype.createHotelsListView = function(hotels){
+  console.log("Hotels View invoked");
+
+}
+
+
+module.exports = ResultsView;
