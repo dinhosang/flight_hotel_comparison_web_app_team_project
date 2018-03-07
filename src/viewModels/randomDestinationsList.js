d272575ddff3842d087228628f0ac58b0cce9a2f
Douglas Sangster <d.sangster@hotmail.co.uk>
2018-03-07 19:40:14 +0000

    modified randomDestination list item tile to be a button an not a ul, added a new ul it is contained within which also now holds a header for the flights. Flights and header are now attached to this container ul. Added checks to handle clearing of elements when clicking from destination to destination and double clicking the same destination

--- src/viewModels/randomDestinationsList.js
+++ src/viewModels/randomDestinationsList.js
@@ -1,5 +1,5 @@
 const RandomDestinationsList = function(options) {
-  
+
   this.depart   = options.destinations[0].departure_date;
   this.return   = options.destinations[0].return_date;
   this.destinations       = options.destinations;
@@ -8,6 +8,8 @@ const RandomDestinationsList = function(options) {
   this.parent = options.parent;
   this.onDestinationClick   = options.callback;
   this.parentObjectInstance = options.parentObject;
+
+  this.activeDestination = null;
 
   this.createSearchResultView();
   this.addTitle();
@@ -33,65 +35,90 @@ RandomDestinationsList.prototype.populateView = function() {
 }
 
 RandomDestinationsList.prototype.addDestination = function(details, index) {
-  const destinationUl = document.createElement('ul');
-  destinationUl.classList.add('random-destination-item');
+  const destinationUl = document.createElement('ul')
+  const destinationButton = document.createElement('button');
+
+  destinationButton.classList.add('random-destination-item');
+  destinationUl.classList.add('random-destination-container')
   destinationUl.id = `random-destination-ul-${index}`
 
-  const radioButton = document.createElement('input');
-  radioButton.type  = 'radio';
-  radioButton.name  = 'random-destination'
-  radioButton.id    = `random-destination-${index}`
+  destinationButton.innerText = details.destination;
 
-  const radioButtonLabel = document.createElement('label');
-  radioButtonLabel.innerText = details.destination;
-  radioButtonLabel.setAttribute('for', radioButton.id);
+  // const radioButton = document.createElement('input');
+  // radioButton.type  = 'radio';
+  // radioButton.name  = 'random-destination'
+  // radioButton.id    = `random-destination-${index}`
+  //
+  // const radioButtonLabel = document.createElement('label');
+  // radioButtonLabel.innerText = details.destination;
+  // radioButtonLabel.setAttribute('for', radioButton.id);
 
-  destinationUl.appendChild(radioButton);
-  destinationUl.appendChild(radioButtonLabel);
+  // destinationUl.appendChild(radioButton);
+  // destinationUl.appendChild(radioButtonLabel);
+  destinationUl.appendChild(destinationButton)
 
-  const options = {
-    parentTile: destinationUl,
-    callback:   this.onDestinationClick,
-    radio: radioButton
-  }
-  radioButton.addEventListener('click', this.prepareFlightsView.bind(this, options));
 
+  const intermediaryCallback = function() {
+
+    const options = {
+      parentTile: destinationUl
+    }
+    this.prepareFlightsView(options)
+  }.bind(this)
+
+
+  destinationButton.addEventListener('click', intermediaryCallback);
   this.searchResultView.appendChild(destinationUl);
 }
 
 RandomDestinationsList.prototype.prepareFlightsView = function (options) {
 
   const alreadyClicked = this.checkIfActiveDestination(options.parentTile);
-  if(alreadyClicked){
-    return;
-  }
 
-  if(this.activeDestination !== undefined){
-    const flightsUl = document.getElementById('flights-list');
-    this.activeDestination.removeChild(flightsUl);
+  if(alreadyClicked){
+    this.clearLists();
+    return;
+  } else if(this.activeDestination !== null){
+    this.clearLists();
   }
 
   this.activeDestination = options.parentTile;
+  const flightsHeader = document.createElement('h3');
+  flightsHeader.id    = 'flights-list-header'
+  flightsHeader.innerText = `Flights to ${this.activeDestination.innerText}`;
 
   const flightsListUl = document.createElement('ul');
   flightsListUl.id    = 'flights-list';
 
-  options.parentTile.appendChild(flightsListUl);
-
+  this.activeDestination.appendChild(flightsHeader);
+  this.activeDestination.appendChild(flightsListUl);
   this.onDestinationClick(this);
 }
 
 RandomDestinationsList.prototype.checkIfActiveDestination = function (destinationUl) {
 
-  if(this.activeDestination === undefined) {
+  if(this.activeDestination === null) {
     return false;
   } else {
     return this.activeDestination === destinationUl;
   }
 }
 
-RandomDestinationsList.prototype.populateFlights = function (options) {
+RandomDestinationsList.prototype.clearLists = function () {
+  const flightsHeader = document.getElementById('flights-list-header');
+  const flightsUl     = document.getElementById('flights-list');
+  const hotelsList    = document.getElementsByClassName('hotels-list')[0];
 
+  if(hotelsList !== undefined) {
+    this.parent.removeChild(hotelsList);
+  }
+
+  this.activeDestination.removeChild(flightsHeader);
+  this.activeDestination.removeChild(flightsUl);
+  this.activeDestination = null;
+}
+
+RandomDestinationsList.prototype.populateFlights = function (options) {
   options.flights.forEach(flight => {
     const flightsListUl = document.getElementById('flights-list');
 
@@ -130,7 +157,7 @@ RandomDestinationsList.prototype.populateFlights = function (options) {
       details: flight,
     }
 
-    radioButton.addEventListener('click', listHotels = function(){
+    radioButton.addEventListener('click', function(){
       options.callback(options2)
     })
   })
