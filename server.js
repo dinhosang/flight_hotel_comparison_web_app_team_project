610e4ad6c750d4a7fa40808167d4efc40206633e
Douglas Sangster <d.sangster@hotmail.co.uk>
2018-03-08 21:44:21 +0000

    modified server.js to accept new naming style for controller

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const indexController = require('./src/controllers/indexController.js')
 server.use(parser.json());
 server.use(express.static(`${__dirname}/build`));
 server.use(parser.urlencoded({extended: true}));
-server.use(index_controller);
+server.use(indexController);
 
 server.listen(3000, function(){
   console.log(`Server listening on port ${this.address().port}`);
