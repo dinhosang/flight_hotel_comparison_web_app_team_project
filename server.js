c32b092979e879014463babb93412f2c3757300c
Richard Allison <richard.allison.18@outlook.com>
2018-03-09 16:17:21 +0000

    working on server database, still progress

--- server.js
+++ server.js
@@ -40,10 +40,55 @@ MongoClient.connect('mongodb://localhost:27017', function(err, client) {
       res.status(201);
       res.json(result.ops[0]);
     })
+  })
+
+  server.get('/api/savedFlights/:url', function(req, res) {
+    const savedFlightsCollection  = db.collection('saved_flights')
+    const lowfareSearchUrl        = req.params.url
+
+    savedFlightsCollection.find().toArray(function(err, allFlightSearches) {
+      if(err) {
+        console.log(err);
+        res.status(500);
+        res.send()
+      }
+
+
+      res.json(allFlightSearches);
+
+
+  //     // console.log(allFlightSearches);
+  //     allFlightSearches.forEach(flightSearch => {
+  //       if(flightSearch.url === lowfareSearchUrl){
+  //         const currentTime     = new Date();
+  //         const flightSavedTime = flightSearch.timeStamp;
+  //
+  //         const millisecondToMinutesConverter = (1000 * 60);
+  //
+  //         const timeDifference = currentTime - flightSavedTime;
+  //         const timeDifferenceInMinutes = parseInt(timeDifference / millisecondToMinutesConverter)
+  //
+  //         if(timeDifferenceInMinutes <= 5) {
+  //           const returnValue = {
+  //             withinFiveMinutes: true,
+  //             flights: flightSearch.flights
+  //           }
+  //           res.json(returnValue);
+  //         } else {
+  //           const returnValue = {
+  //             withinFiveMinutes: false,
+  //             flights: flightSearch.url
+  //           }
+  //           res.json(returnValue);
+  //         }
+  //       }
+  //     })
+    })
 
   })
 
 });
+
 
 server.listen(3000, function(){
   console.log(`Server listening on port ${this.address().port}`);
