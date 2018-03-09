52420e29a6e34a8883a5f08e9eaca22cf5ff54b1
Douglas Sangster <d.sangster@hotmail.co.uk>
2018-03-09 22:32:40 +0000

    recentLowfareSearches model now deletes searches that are older than 5 minutes if that same search is made again. Also deletes all searches older than five minutes when model is instantiated. Which means the work done for the start of this comment is not necessary

--- server/dataModels/recentLowfareSearches.js
+++ server/dataModels/recentLowfareSearches.js
@@ -2,6 +2,7 @@ const db = require('../database/mongodb');
 
 const RecentLowfareSearches = function(){
   this.flightSearches = db.connection.collection('saved_lowfare_searches');
+  this.removeAllOlderThanFiveMinutes();
 }
 
 RecentLowfareSearches.prototype.saveSearch = function(lowfareAPIUrl, lowfareAPIDataHash, functionToSendResponse) {
@@ -16,7 +17,7 @@ RecentLowfareSearches.prototype.saveSearch = function(lowfareAPIUrl, lowfareAPID
 }
 
 RecentLowfareSearches.prototype.checkIfRecentSearch = function(lowfareSearchUrl, functionToSendResponse) {
-  this.flightSearches.find().toArray(function(err, allFlightSearches) {
+  this.flightSearches.find().toArray((err, allFlightSearches) => {
     if(err) {
       console.log(err);
       functionToSendResponse(err);
@@ -33,12 +34,9 @@ RecentLowfareSearches.prototype.checkIfRecentSearch = function(lowfareSearchUrl,
 
     let timeDifferenceInMinutes;
     if(savedFlightSearch !== null) {
-      const currentTime     = new Date();
-      const flightSavedTime = savedFlightSearch.searchTime;
-      const millisecondToMinutesConverter = (1000 * 60);
-
-      const timeDifference = currentTime - flightSavedTime;
-      timeDifferenceInMinutes = parseInt(timeDifference / millisecondToMinutesConverter);
+      const currentTime       = new Date();
+      const flightSavedTime   = savedFlightSearch.searchTime;
+      timeDifferenceInMinutes = this.compareDates(currentTime, flightSavedTime)
     }
 
     // needs to be less than 5 as the above calculation seems to drop
@@ -56,9 +54,50 @@ RecentLowfareSearches.prototype.checkIfRecentSearch = function(lowfareSearchUrl,
         withinFiveMinutes: false,
         search: lowfareSearchUrl
       }
+
+      this.removeSearch(lowfareSearchUrl);
       functionToSendResponse(err, returnValue);
     }
   })
 }
+
+RecentLowfareSearches.prototype.removeSearch = function (lowfareSearchUrl) {
+  const filterObject = {url: lowfareSearchUrl};
+
+  this.flightSearches.deleteMany(filterObject, function(err, result) {
+    if(err) {
+      console.log(err);
+    }
+
+    console.log(result);
+  })
+}
+
+RecentLowfareSearches.prototype.removeAllOlderThanFiveMinutes = function () {
+  this.flightSearches.find().toArray((err, allSearches) => {
+    if(err) {
+      console.log(err);
+      return;
+    }
+
+    allSearches.forEach(search => {
+      const timeNow = new Date();
+      const timeSearchWasSaved = new Date(search.searchTime);
+      const timeDifferenceInMinutes = this.compareDates(timeNow, timeSearchWasSaved)
+      if(timeDifferenceInMinutes >= 5) {
+        this.removeSearch(search.url)
+      }
+    });
+  });
+}
+
+RecentLowfareSearches.prototype.compareDates = function (firstTime, secondTime) {
+  const millisecondToMinutesConverter = (1000 * 60);
+
+  const timeDifference          = firstTime - secondTime;
+  const timeDifferenceInMinutes = parseInt(timeDifference / millisecondToMinutesConverter);
+
+  return timeDifferenceInMinutes;
+};
 
 module.exports = RecentLowfareSearches;
