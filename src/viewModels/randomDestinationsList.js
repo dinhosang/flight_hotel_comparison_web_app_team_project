046bdada971a974d39f7aa22a3c520701f41054d
Douglas Sangster <d.sangster@hotmail.co.uk>
2018-03-10 15:00:31 +0000

    refactored flights list to display information in tables

--- src/viewModels/randomDestinationsList.js
+++ src/viewModels/randomDestinationsList.js
@@ -1,3 +1,7 @@
+const currencyEnum        = require('../helpers/enums/currencyListEnum');
+const airportIataConstant = require('../helpers/enums/iataAirportsEnum');
+const cityIataConstant    = require('../helpers/enums/iataCitiesEnum');
+
 const RandomDestinationsList = function(dataForListingDestinations) {
 
   this.depart               = dataForListingDestinations.destinations[0].departure_date;
@@ -123,8 +127,8 @@ RandomDestinationsList.prototype.clearLists = function () {
 }
 
 RandomDestinationsList.prototype.populateFlights = function(details) {
-  const flightsListHeader = document.createElement('h3');
-  flightsListHeader.id    = 'flights-list-header'
+  const flightsListHeader     = document.createElement('h3');
+  flightsListHeader.id        = 'flights-list-header';
   flightsListHeader.innerText = `Flights to ${this.activeDestination.innerText}`;
 
   const flightsListUl = document.createElement('ul');
@@ -132,82 +136,209 @@ RandomDestinationsList.prototype.populateFlights = function(details) {
 
   this.activeDestination.appendChild(flightsListHeader);
   this.activeDestination.appendChild(flightsListUl);
-
+  let mostRecentPrice = null;
   details.flights.forEach(flight => {
+    if(mostRecentPrice === null || mostRecentPrice !== flight.fare.total_price) {
+      const options = {
+        callback: details.callback,
+        flightDetails: flight,
+        currency: details.currency,
+        header: flightsListHeader,
+        list: flightsListUl
+      }
 
-    const options = {
-      callback: details.callback,
-      flightDetails: flight,
-      currency: details.currency,
-      header: flightsListHeader,
-      list: flightsListUl
+      mostRecentPrice = flight.fare.total_price;
+      this.addFlight(options)
     }
-
-    this.addFlight(options)
   })
 }
 
 RandomDestinationsList.prototype.addFlight = function(details) {
 
-  const callback  = details.callback;
-  const flight    = details.flightDetails;
+  const callback      = details.callback;
+  const currencyCode  = details.currency;
+  const flight        = details.flightDetails;
   const flightsListUl = details.list;
 
-  const flightTile = document.createElement('button');
+  const flightTile    = document.createElement('button');
   flightTile.classList.add('flight-details-selection-item');
-
-  const flightDetailsUl = document.createElement('ul');
+  const flightDetailsUl       = document.createElement('ul');
 
   const outboundFlightsArray  = flight.itineraries[0].outbound.flights;
   const arrayLength           = outboundFlightsArray.length;
+  const firstLegOfOutbound    = outboundFlightsArray[0];
   const finalOutboundFlight   = outboundFlightsArray[arrayLength -1];
   const finalDestinationIata  = finalOutboundFlight.destination.airport;
 
-  const airportIataEnum = require('../helpers/enums/iataAirportsEnum');
-  const airportHash = airportIataEnum.BYIATAAIRPORT[finalDestinationIata]
+
+  const airportHash = airportIataConstant.BYIATAAIRPORT[finalDestinationIata]
   const airportName = airportHash.nameAirport;
   const countryCode = airportHash.codeIso2Country;
+  const originAirportIata = firstLegOfOutbound.origin.airport;
+  const originAirportHash = airportIataConstant.BYIATAAIRPORT[originAirportIata];
+  const originAirportName = originAirportHash.nameAirport;
+  const originCityIata    = originAirportHash.codeIataCity;
+  const originCityName    = cityIataConstant.BYIATACITY[originCityIata].nameCity;
+  const originCountryCode = originAirportHash.codeIso2Country;
 
-  const nameLi = document.createElement('li');
-  nameLi.innerText = `Destination Airport: ${airportName}`
-  flightDetailsUl.appendChild(nameLi);
-  // nameLi.innerText = flight.itineraries[0].outbound.flights[0].destination.airport;
+  const flightDetailHeader  = document.createElement('h3');
+  const destinationCityIata = airportIataConstant.BYIATAAIRPORT[finalDestinationIata].codeIataCity;
+  const cityName = cityIataConstant.BYIATACITY[destinationCityIata].nameCity;
 
-  const priceLi = document.createElement('li');
-  const currencyCode    = details.currency;
-  const currencyEnum    = require('../helpers/enums/currencyListEnum');
-  const currencySymbol  = currencyEnum[currencyCode].symbol;
-
+  const currencySymbol      = currencyEnum[currencyCode].symbol;
   if(currencySymbol !== currencyCode){
-    priceLi.innerText = `Price: ${currencySymbol}${flight.fare.total_price}`;
+    flightDetailHeader.innerText = `${cityName}, ${countryCode} - ${currencySymbol}${flight.fare.total_price}`;
   } else {
-    priceLi.innerText = `Price: ${flight.fare.total_price} ${currencySymbol}`;
+    flightDetailHeader.innerText = `${cityName}, ${countryCode} - ${flight.fare.total_price} ${currencySymbol}`;
   }
-  flightDetailsUl.appendChild(priceLi);
+  flightDetailsUl.appendChild(flightDetailHeader);
 
 
-  const outboundStopDisplay = document.createElement('li');
-  const outboundStopCount   = outboundFlightsArray.length;
-  outboundStopDisplay.innerText = `Outbound Stops: ${outboundStopCount}`;
-  if(outboundStopCount !== 1) {
-    flightDetailsUl.appendChild(outboundStopDisplay);
-  }
+  const flightDetailsTable  = document.createElement('table');
+
+  const outboundRowOne      = document.createElement('tr');
+  const outboundRowHead     = document.createElement('td');
+  outboundRowHead.setAttribute('rowspan', '5');
+  outboundRowHead.classList.add('main-column');
+  outboundRowHead.innerText = 'Outbound';
+  const outboundDestination = document.createElement('td');
+  outboundDestination.classList.add('sub-main-column');
+  outboundDestination.innerText   = 'Destination Airport:';
+  const outboundDestinationValue  = document.createElement('td');
+  outboundDestinationValue.classList.add('info-column');
+  outboundDestinationValue.innerText = `${airportName}, ${cityName}, ${countryCode}`;
+  outboundRowOne.appendChild(outboundRowHead);
+  outboundRowOne.appendChild(outboundDestination);
+  outboundRowOne.appendChild(outboundDestinationValue);
+  flightDetailsTable.appendChild(outboundRowOne);
+
+  const outboundRowTwo          = document.createElement('tr');
+  const outboundDepartTime      = document.createElement('td');
+  outboundDepartTime.classList.add('sub-main-colum');
+  outboundDepartTime.innerText  = 'Depart Time:';
+  const outboundDepartTimeValue = document.createElement('td');
+  outboundDepartTimeValue.classList.add('info-column');
+  const departTime              = firstLegOfOutbound.departs_at
+  const departTimeArray         = departTime.split('T');
+  const departTimeFormatted     = departTimeArray.join(', ');
+  outboundDepartTimeValue.innerText = `${departTimeFormatted}`;
+  outboundRowTwo.appendChild(outboundDepartTime);
+  outboundRowTwo.appendChild(outboundDepartTimeValue);
+  flightDetailsTable.appendChild(outboundRowTwo);
+
+  const outboundRowThree          = document.createElement('tr');
+  const outboundArrivalTime       = document.createElement('td');
+  outboundArrivalTime.classList.add('sub-main-column');
+  outboundArrivalTime.innerText   = 'Arrival Time:';
+  const outboundArrivalTimeValue  = document.createElement('td');
+  outboundArrivalTimeValue.classList.add('info-column');
+  const arrivalTime           = finalOutboundFlight.arrives_at;
+  const arrivalTimeArray      = arrivalTime.split('T');
+  const arrivalTimeFormatted  = arrivalTimeArray.join(', ');
+  outboundArrivalTimeValue.innerText = `${arrivalTimeFormatted}`;
+  outboundRowThree.appendChild(outboundArrivalTime);
+  outboundRowThree.appendChild(outboundArrivalTimeValue);
+  flightDetailsTable.appendChild(outboundRowThree);
+
+  const outboundRowFour             = document.createElement('tr');
+  const outboundSeatClass           = document.createElement('td');
+  outboundSeatClass.classList.add('sub-main-column');
+  outboundSeatClass.innerText       = 'Seat Type:';
+  const outboundSeatClassValue      = document.createElement('td');
+  outboundSeatClassValue.classList.add('info-column');
+  const seatClass = firstLegOfOutbound.booking_info.travel_class;
+  outboundSeatClassValue.innerText  = `${seatClass}`;
+  outboundRowFour.appendChild(outboundSeatClass);
+  outboundRowFour.appendChild(outboundSeatClassValue);
+  flightDetailsTable.appendChild(outboundRowFour);
+
+  const outboundRowFive             = document.createElement('tr');
+  const outboundStopCount           = document.createElement('td');
+  outboundStopCount.classList.add('sub-main-column');
+  outboundStopCount.innerText       = 'Stop Count:';
+  const outboundStopCountValue      = document.createElement('td');
+  outboundStopCountValue.classList.add('info-column');
+  const outboundStopCountInt        = outboundFlightsArray.length;
+  outboundStopCountValue.innerText  = `${outboundStopCountInt}`;
+  outboundRowFive.appendChild(outboundStopCount);
+  outboundRowFive.appendChild(outboundStopCountValue);
+  flightDetailsTable.appendChild(outboundRowFive);
 
 
-  const inboundStopDisplay  = document.createElement('li');
-  const inboundExists = flight.itineraries[0].inbound !== undefined;
-  let inboundStopCount
-  if(inboundExists) {
-    const inboundFlightsArray = flight.itineraries[0].inbound.flights;
-    inboundStopCount          = inboundFlightsArray.length;
-    inboundStopDisplay.innerText = `Inbound Stops: ${inboundStopCount}`;
-  }
+  const inboundFlightsArray   = flight.itineraries[0].inbound.flights;
+  const inboundarrayLength    = inboundFlightsArray.length;
+  const firstLegOfInbound     = inboundFlightsArray[0];
+  const finalInboundFlight    = inboundFlightsArray[arrayLength -1];
 
-  if(inboundExists && (outboundStopCount !== 1 || inboundStopCount !== 1)) {
-    flightDetailsUl.appendChild(inboundStopDisplay);
-  }
+  const inboundRowOne       = document.createElement('tr');
+  const inboundRowHead      = document.createElement('td');
+  inboundRowHead.setAttribute('rowspan', '5');
+  inboundRowHead.classList.add('main-column');
+  inboundRowHead.innerText  = 'Inbound';
+  const inboundDestination = document.createElement('td');
+  inboundDestination.classList.add('sub-main-column');
+  inboundDestination.innerText      = 'Destination Airport:';
+  const inboundDestinationValue    = document.createElement('td');
+  inboundDestinationValue.classList.add('info-column');
+  inboundDestinationValue.innerText = `${originAirportName}, ${originCityName}, ${originCountryCode}`;
+  inboundRowOne.appendChild(inboundRowHead);
+  inboundRowOne.appendChild(inboundDestination);
+  inboundRowOne.appendChild(inboundDestinationValue);
+  flightDetailsTable.appendChild(inboundRowOne);
 
+  const inboundRowTwo          = document.createElement('tr');
+  const inboundDepartTime      = document.createElement('td');
+  inboundDepartTime.classList.add('sub-main-colum');
+  inboundDepartTime.innerText  = 'Depart Time:';
+  const inboundDepartTimeValue = document.createElement('td');
+  inboundDepartTimeValue.classList.add('info-column');
+  const inboundDepartTimeFromHash   = firstLegOfInbound.departs_at
+  const inboundDepartTimeArray      = inboundDepartTimeFromHash.split('T');
+  const inboundDepartTimeFormatted  = inboundDepartTimeArray.join(', ');
+  inboundDepartTimeValue.innerText  = `${inboundDepartTimeFormatted}`;
+  inboundRowTwo.appendChild(inboundDepartTime);
+  inboundRowTwo.appendChild(inboundDepartTimeValue);
+  flightDetailsTable.appendChild(inboundRowTwo);
 
+  const inboundRowThree          = document.createElement('tr');
+  const inboundArrivalTime       = document.createElement('td');
+  inboundArrivalTime.classList.add('sub-main-column');
+  inboundArrivalTime.innerText   = 'Arrival Time:';
+  const inboundArrivalTimeValue  = document.createElement('td');
+  inboundArrivalTimeValue.classList.add('info-column');
+  const inboundArrivalTimeFromHash   = finalInboundFlight.arrives_at;
+  const inboundArrivalTimeArray      = inboundArrivalTimeFromHash.split('T');
+  const inboundArrivalTimeFormatted  = inboundArrivalTimeArray.join(', ');
+  inboundArrivalTimeValue.innerText = `${inboundArrivalTimeFormatted}`;
+  inboundRowThree.appendChild(inboundArrivalTime);
+  inboundRowThree.appendChild(inboundArrivalTimeValue);
+  flightDetailsTable.appendChild(inboundRowThree);
+
+  const inboundRowFour             = document.createElement('tr');
+  const inboundSeatClass           = document.createElement('td');
+  inboundSeatClass.classList.add('sub-main-column');
+  inboundSeatClass.innerText       = 'Seat Type:';
+  const inboundSeatClassValue      = document.createElement('td');
+  inboundSeatClassValue.classList.add('info-column');
+  const inboundseatClass = firstLegOfInbound.booking_info.travel_class;
+  inboundSeatClassValue.innerText  = `${inboundseatClass}`;
+  inboundRowFour.appendChild(inboundSeatClass);
+  inboundRowFour.appendChild(inboundSeatClassValue);
+  flightDetailsTable.appendChild(inboundRowFour);
+
+  const inboundRowFive             = document.createElement('tr');
+  const inboundStopCount           = document.createElement('td');
+  inboundStopCount.classList.add('sub-main-column');
+  inboundStopCount.innerText       = 'Stop Count:';
+  const inboundStopCountValue      = document.createElement('td');
+  inboundStopCountValue.classList.add('info-column');
+  const inboundStopCountInt        = inboundFlightsArray.length;
+  inboundStopCountValue.innerText  = `${inboundStopCountInt}`;
+  inboundRowFive.appendChild(inboundStopCount);
+  inboundRowFive.appendChild(inboundStopCountValue);
+  flightDetailsTable.appendChild(inboundRowFive);
+
+  flightDetailsUl.appendChild(flightDetailsTable);
   flightTile.appendChild(flightDetailsUl);
   flightsListUl.appendChild(flightTile);
 
